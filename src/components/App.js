35a8fa088aa42e78ece78bd70346ffa678f47556
Kaname87 <kaname.nakanishi@bibo.com.ph>
2018-07-07 21:13:43 +0800

    Remove unncessary component

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import JobGuidanceApp from "./JobGuidanceApp";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import JobList from "./JobList";
+import NavBar from "./NavBar";
 
 class App extends Component {
   render() {
     return (
       <Router>
-        <Route path="/" component={JobGuidanceApp} />
+        <div>
+          <NavBar />
+          <Route path="/" component={JobList} />
+        </div>
       </Router>
     );
   }
