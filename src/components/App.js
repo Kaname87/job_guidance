88053645fb52aec5f2571016794b0837c1d54667
Kaname87 <kaname.nakanishi@bibo.com.ph>
2018-07-08 18:32:30 +0800

    Add ScrollTopTop HOC

--- src/components/App.js
+++ src/components/App.js
@@ -3,16 +3,19 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import JobList from "./JobList";
 import JobDetail from "./JobDetail";
 import NavBar from "./NavBar";
+import ScrollToTop from "./ScrollToTop";
 
 class App extends Component {
   render() {
     return (
       <Router>
-        <div>
-          <NavBar />
-          <Route exact path="/" component={JobList} />
-          <Route path="/job_detail/:id" component={JobDetail} />
-        </div>
+        <ScrollToTop>
+          <div>
+            <NavBar />
+            <Route exact path="/" component={JobList} />
+            <Route path="/job_detail/:id" component={JobDetail} />
+          </div>
+        </ScrollToTop>
       </Router>
     );
   }
